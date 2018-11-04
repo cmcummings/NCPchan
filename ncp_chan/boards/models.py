5ba3c282495593de0cb52d44ef826c5d8c952fe7
gestalt8003 <cmcummings2003@gmail.com>
2018-11-03 20:35:03 -0500

    update

--- ncp_chan/boards/models.py
+++ ncp_chan/boards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Thread(models.Model):
+    board = models.CharField(max_length=5)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=20)
+    date = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    # TODO add images
+
+class Reply(models.Model):
+    author = models.CharField(max_length=20)
+    date = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    # TODO add images
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
--- ncp_chan/boards/views.py
+++ ncp_chan/boards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Thread, Reply
 
 def home(request):
     return render(request, 'home/home.html')
@@ -8,6 +9,40 @@ def wel(request):
         'board': {
             'extension': '/wel/',
             'description': 'Welcome/Introductions'
+        },
+        'threads': {
+            'thread_id': {
+                'title': 'title',
+                'author': 'author',
+                'date': 'date',
+                'content': 'content',
+                'replies': {
+                    'reply_id': {
+                        'author': 'author',
+                        'date': 'date',
+                        'content': 'content'
+                    }
+                }
+            }
         }
     }
+    threads_context = context['threads']
+    threads = Thread.objects.all()
+
+    for thread in threads:
+        threads_context[str(thread.id)] = {
+            'title': thread.title,
+            'author': thread.author,
+            'date': thread.date,
+            'content': thread.content,
+            'replies': {}
+        }
+        replies = thread.reply_set.all()
+        for reply in replies:
+            threads_context[str(thread.id)]['replies'][str(reply.id)] = {
+                'author': reply.author,
+                'date': reply.date,
+                'content': reply.content
+            }
+    
     return render(request, 'boards/wel.html', context)
